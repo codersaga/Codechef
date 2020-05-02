0330ee32609835388c425181957ff148c8f5dad3
codersaga <64676906+codersaga@users.noreply.github.com>
2020-05-02 12:03:16 +0530

    Add files via upload

--- Practice(Beginner)/ATM.c
+++ Practice(Beginner)/ATM.c
@@ -1,1 +1,20 @@
-
+#include<stdio.h>
+ 
+int main()
+{
+               int withdraw;
+               float tot_bal;
+               float new_bal;
+               scanf("%d",&withdraw);
+               scanf("%f",&tot_bal);
+               if(withdraw>=tot_bal-0.5 || withdraw % 5!=0)
+               {
+                       printf("%.2f",tot_bal);
+               }
+               else
+               {
+                               new_bal=tot_bal-withdraw-0.5;
+                               printf("%.2f",new_bal);
+               }
+               return 0;
+}
